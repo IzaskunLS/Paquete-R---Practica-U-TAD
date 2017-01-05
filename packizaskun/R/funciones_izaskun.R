c9972e1202131a408635b3071e773cac0545cd96
IzaskunLS <izaskun.ls@gmail.com>
2017-01-05 14:24:31 +0100

    nueva version

---
+++ packizaskun/R/funciones_izaskun.R
@@ -0,0 +1,15 @@
+
+suma.dos.numeros <- function(x, y){
+  resultado <- x + y
+  return(resultado)
+}
+
+suma.tres.numeros <- function(x, y, z){
+  resultado <- x + y + z
+  return(resultado)
+}
+
+resta.dos.numeros <- function(x, y){
+  resultado <- x - y
+  return(resultado)
+}
